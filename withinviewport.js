167c90c4c7706211faaac5fd164a82cb1e3d1842
Craig Patik <craig@patik.com>
2015-08-02 13:43:30 -0400

    Version bump to 1.0.0

--- withinviewport.js
+++ withinviewport.js
@@ -3,8 +3,8 @@
  *
  * @description Determines whether an element is completely within the browser viewport
  * @author      Craig Patik, http://patik.com/
- * @version     0.1.0
- * @date        2015-04-10
+ * @version     1.0.0
+ * @date        2015-08-02
  */
 (function (root, name, factory) {
     // AMD
@@ -40,7 +40,6 @@
         var metadata = {};
         var config = {};
         var settings;
-        var useHtmlElem;
         var isWithin;
         var elemBoundingRect;
         var sideNamesPattern;
@@ -70,48 +69,45 @@
             settings = options || {};
         }
 
-        // Build configuration from defaults and given settings
-        config.container = settings.container || metadata.container || withinviewport.defaults.container || document.body;
+        // Build configuration from defaults and user-provided settings and metadata
+        config.container = settings.container || metadata.container || withinviewport.defaults.container || window;
         config.sides  = settings.sides  || metadata.sides  || withinviewport.defaults.sides  || 'all';
         config.top    = settings.top    || metadata.top    || withinviewport.defaults.top    || 0;
         config.right  = settings.right  || metadata.right  || withinviewport.defaults.right  || 0;
         config.bottom = settings.bottom || metadata.bottom || withinviewport.defaults.bottom || 0;
         config.left   = settings.left   || metadata.left   || withinviewport.defaults.left   || 0;
 
-        // Whether we can use the `<html`> element for `scrollTop`
-        // Unfortunately at the moment I can't find a way to do this without UA-sniffing
-        useHtmlElem = !/Chrome/.test(navigator.userAgent);
+        // Use the window as the container if the user specified the body or a non-element
+        if (!(config.container instanceof HTMLElement) || config.container === document.body) {
+            config.container = window;
+        }
 
         // Element testing methods
         isWithin = {
             // Element is below the top edge of the viewport
-            top: function _isWithin_top() {
+            top: function _isWithin_top () {
                 return elemBoundingRect.top >= config.top;
             },
 
             // Element is to the left of the right edge of the viewport
-            right: function _isWithin_right() {
-                var container = (config.container === document.body) ? window : config.container;
-
+            right: function _isWithin_right () {
                 // Note that `elemBoundingRect.right` is the distance from the *left* of the viewport to the element's far right edge
-                return elemBoundingRect.right <= container.innerWidth - config.right;
+                return elemBoundingRect.right <= config.container.innerWidth - config.right;
             },
 
             // Element is above the bottom edge of the viewport
-            bottom: function _isWithin_bottom() {
-                var container = (config.container === document.body) ? window : config.container;
-
+            bottom: function _isWithin_bottom () {
                 // Note that `elemBoundingRect.bottom` is the distance from the *top* of the viewport to the element's bottom edge
-                return elemBoundingRect.bottom <= container.innerHeight - config.bottom;
+                return elemBoundingRect.bottom <= config.container.innerHeight - config.bottom;
             },
 
             // Element is to the right of the left edge of the viewport
-            left: function _isWithin_left() {
+            left: function _isWithin_left () {
                 return elemBoundingRect.left >= config.left;
             },
 
             // Element is within all four boundaries
-            all: function _isWithin_all() {
+            all: function _isWithin_all () {
                 // Test each boundary in order of most efficient and most likely to be false so that we can avoid running all four functions on most elements
                 // Top: Quickest to calculate + most likely to be false
                 // Bottom: Note quite as quick to calculate, but also very likely to be false
@@ -167,19 +163,19 @@
 
     // Shortcut methods for each side of the viewport
     // Example: `withinviewport.top(elem)` is the same as `withinviewport(elem, 'top')`
-    withinviewport.prototype.top = function _withinviewport_top(element) {
+    withinviewport.prototype.top = function _withinviewport_top (element) {
         return withinviewport(element, 'top');
     };
 
-    withinviewport.prototype.right = function _withinviewport_right(element) {
+    withinviewport.prototype.right = function _withinviewport_right (element) {
         return withinviewport(element, 'right');
     };
 
-    withinviewport.prototype.bottom = function _withinviewport_bottom(element) {
+    withinviewport.prototype.bottom = function _withinviewport_bottom (element) {
         return withinviewport(element, 'bottom');
     };
 
-    withinviewport.prototype.left = function _withinviewport_left(element) {
+    withinviewport.prototype.left = function _withinviewport_left (element) {
         return withinviewport(element, 'left');
     };
 
