2f914660b6adb83404fab4dd684f751f20649c7f
Craig Patik <craig@patik.com>
2023-01-13 22:09:55 +0100

    cleanup

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'node', 'prettier'],
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:node/recommended',
+        "plugin:react-hooks/recommended",
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        "plugin:jsx-a11y/recommended"
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        'prettier/prettier': 'warn',
+        'node/no-missing-import': 'off',
+        'node/no-empty-function': 'off',
+        'node/no-unsupported-features/es-syntax': 'off',
+        'node/no-missing-require': 'off',
+        'node/shebang': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        quotes: ['warn', 'single', { avoidEscape: true }],
+        'node/no-unpublished-import': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+    },
+    "env": {
+        "jest": true,
+        "node": true
+    },
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    },
+}
