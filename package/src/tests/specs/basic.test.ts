498545de9cdc8d72d5023e73c1a6ac18582637d0
Craig Patik <craig@patik.com>
2023-01-14 13:08:59 +0100

    some more work on tests but it's not really working out

--- package/src/tests/specs/basic.test.ts
+++ package/src/tests/specs/basic.test.ts
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/dom'
+import { withinviewport } from '../../../dist/index'
 
 test('uses jest-dom', () => {
     document.body.innerHTML = `
@@ -8,4 +9,62 @@ test('uses jest-dom', () => {
 
     expect(screen.queryByTestId('not-empty')).not.toBeEmptyDOMElement()
     expect(screen.getByText('Visible Example')).toBeVisible()
+})
+
+describe('uses within-viewport', () => {
+    test('basic', () => {
+        document.body.innerHTML = `
+  <span data-testid="not-empty"><span data-testid="empty"></span></span>
+  <div data-testid="visible">Visible Example</div>
+`
+
+        expect(screen.queryByTestId('not-empty')).not.toBeEmptyDOMElement()
+
+        const elem = screen.getByText('Visible Example')
+        expect(elem).toBeVisible()
+
+        expect(withinviewport(elem)).toBe(true)
+    })
+
+    test('out of viewport', () => {
+        document.body.innerHTML = `
+<span data-testid="not-empty"><span data-testid="empty"></span></span>
+<div data-testid="tall" style="height: 2000px;">Tall div</div>
+<div data-testid="tall" style="min-height: 2000px;">Tall div</div>
+<div data-testid="visible">Visible Example</div>
+`
+
+        Object.defineProperty(window, 'innerHeight', {
+            writable: true,
+            configurable: true,
+            value: 150,
+        })
+
+        window.dispatchEvent(new Event('resize'))
+
+        expect(screen.queryByTestId('not-empty')).not.toBeEmptyDOMElement()
+
+        // const elem = screen.getByText('Visible Example')
+        // expect(elem).toBeVisible()
+
+        const elem: HTMLElement | null = window.document.querySelector('[data-testid="visible"]')
+
+        if (elem) {
+            expect(withinviewport(elem)).toBe(false)
+        }
+    })
+})
+
+describe('test window height', () => {
+    it('test the height', () => {
+        Object.defineProperty(window, 'innerHeight', {
+            writable: true,
+            configurable: true,
+            value: 150,
+        })
+
+        window.dispatchEvent(new Event('resize'))
+
+        expect(window.innerHeight).toBe(150)
+    })
 })
