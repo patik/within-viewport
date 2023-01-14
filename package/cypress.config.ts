9c0b1d1ff39745b803c5a96102f81c7444e572cd
Craig Patik <craig@patik.com>
2023-01-14 17:07:05 +0100

    combined legacy and modern packages into one project

--- package/cypress.config.ts
+++ package/cypress.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-    projectId: 'oxu623',
     e2e: {
         supportFile: false,
         setupNodeEvents(/* on, config */) {
